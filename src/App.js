dc79448e733e7622f060c9582612944f9bc2a010
Assay Poulose <94008297+assaypoulose@users.noreply.github.com>
2024-06-13 12:27:04 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import Signup from './components/Signup';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import Login from './components/Login';
+import { useEffect, useState } from 'react'; // Import useState
+import { useSelector, useDispatch } from 'react-redux';
+import io from 'socket.io-client';
+import { setOnlineUsers } from './redux/userSlice';
+import { BASE_URL } from '.';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+  {
+    path: '/register',
+    element: <Signup />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+]);
 
 function App() {
+  const { authUser } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+  const [socket, setSocket] = useState(null); // Local state for socket
+
+  useEffect(() => {
+    if (authUser) {
+      const socketio = io(`${BASE_URL}`, {
+        query: {
+          userId: authUser._id,
+        },
+      });
+      setSocket(socketio); // Set local state
+
+      socketio.on('getOnlineUsers', (onlineUsers) => {
+        dispatch(setOnlineUsers(onlineUsers));
+      });
+
+      return () => {
+        socketio.close();
+        setSocket(null); // Clean up local state
+      };
+    } else {
+      if (socket) {
+        socket.close();
+        setSocket(null); // Clean up local state
+      }
+    }
+  }, [authUser, dispatch]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="p-4 h-screen flex items-center justify-center">
+      <RouterProvider router={router} />
     </div>
   );
 }
